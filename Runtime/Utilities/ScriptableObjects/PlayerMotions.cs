ac5514361682f4e295e13e3fd4aaa1e8e9d5aae4
Christian Heider Nielsen <mrnaah@gmail.com>
2019-01-14 14:25:02 +0100

    Tutorial

--- Runtime/Utilities/ScriptableObjects/PlayerMotions.cs
+++ Runtime/Utilities/ScriptableObjects/PlayerMotions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Neodroid.Runtime.Utilities.Misc.SearchableEnum;
+using UnityEditor.UI;
 using UnityEngine;
 
 namespace Neodroid.Runtime.Utilities.ScriptableObjects {
@@ -14,6 +15,24 @@ namespace Neodroid.Runtime.Utilities.ScriptableObjects {
     /// <summary>
     /// </summary>
     public PlayerMotion[] _Motions;
+
+    void OnValidate() {
+/*
+      foreach (var motion in this._Motions) {
+        foreach (var motion2 in this._Motions) {
+          if (motion._Key == motion2._Key) {
+            Debug.LogWarning($"{motion} and {motion2} has the same Key");
+          }
+        }
+      }
+  */
+      var copy = this._Motions;
+      for (var i = 0; i < copy.Length; i++) {
+        copy[i]._Actor = copy[i]._Actor.Trim(' ');
+        copy[i]._Motor = copy[i]._Motor.Trim(' ');
+      }
+      this._Motions = copy;
+    }
   }
 
   /// <summary>
