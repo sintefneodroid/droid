89e0998ab11f2c823288e4a2c9d55acf12f0d221
cnheider <mrnaah@gmail.com>
2019-02-07 22:00:23 +0100

    Ecs test

---
+++ Runtime/EcsPrototyping/HelloRotationSpeedComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine.Serialization;
+
+namespace droid.Runtime.EcsPrototyping
+{
+// Serializable attribute is for editor support.
+    [Serializable]
+    public struct HelloRotationSpeed : IComponentData
+    {
+       public float rotationalMultiplier;
+    }
+
+// ComponentDataWrapper is for creating a Monobehaviour representation of this component (for editor support).
+    [UnityEngine.DisallowMultipleComponent]
+    public class HelloRotationSpeedComponent : ComponentDataWrapper<HelloRotationSpeed> { }
+}
