bc9e5c350e2f17c405183db2f86d0995dbf149f0
cnheider <mrnaah@gmail.com>
2019-03-19 17:06:54 +0100

    Ecs

---
+++ Runtime/EcsPrototyping/Components/Sensors/CameraSensorComponent.cs
@@ -0,0 +1,19 @@
+﻿#if ECS_EXISTS
+
+using System;
+using Unity.Entities;
+
+namespace droid.Runtime.EcsPrototyping.Components.Sensors
+{
+// Serializable attribute is for editor support.
+    [Serializable]
+    public struct HelloRotationSpeed : IComponentData
+    {
+       public float rotationalMultiplier;
+    }
+
+// ComponentDataWrapper is for creating a Monobehaviour representation of this component (for editor support).
+    [UnityEngine.DisallowMultipleComponent]
+    public class HelloRotationSpeedComponent : ComponentDataWrapper<HelloRotationSpeed> { }
+}
+#endif
---
+++ Runtime/EcsPrototyping/Systems/CamersSensorSystem.cs
@@ -0,0 +1,42 @@
+﻿#if ECS_EXISTS
+using droid.Runtime.EcsPrototyping.Components.Sensors;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+// This system updates all entities in the scene with both a HelloRotationSpeed and Rotation component.
+namespace droid.Runtime.EcsPrototyping.Systems
+{
+    public class HelloRotationSpeedSystem : JobComponentSystem
+    {
+        // Use the [BurstCompile] attribute to compile a job with Burst. You may see significant speed ups, so try it!
+        [BurstCompile]
+        struct HelloRotationSpeedJob : IJobProcessComponentData<Rotation, HelloRotationSpeed>
+        {
+            // The [ReadOnly] attribute tells the job scheduler that this job cannot write to dT.
+            [ReadOnly] public float DeltaT;
+
+            // rotation is read/write in this job
+            public void Execute(ref Rotation rotation, [ReadOnly] ref HelloRotationSpeed rotSpeed)
+            {
+                // Rotate something about its up vector at the speed given by HelloRotationSpeed.
+                rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), rotSpeed.rotationalMultiplier * this.DeltaT));
+            }
+        }
+
+        // OnUpdate runs on the main thread.
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var job = new HelloRotationSpeedJob(){
+                DeltaT = Time.deltaTime
+            };
+
+            return job.Schedule(this, inputDependencies);
+        }
+    }
+}
+#endif
