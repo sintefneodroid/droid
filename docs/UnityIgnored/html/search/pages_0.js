ff7c39afe66d52d1f3b695153bcf8f26aac8676e
cnheider <mrnaah@gmail.com>
2019-06-08 17:39:14 +0200

    Unity Ignore Docs

---
+++ docs/UnityIgnored/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['changelog',['CHANGELOG',['../md__home_heider__projects__neodroid__unity__examples__assets_droid__runtime__c_h_a_n_g_e_l_o_g.html',1,'']]]
+];
--- docs/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['license',['LICENSE',['../md__runtime__l_i_c_e_n_s_e.html',1,'']]]
-];
